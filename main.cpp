8e69e62f12b0f4bef92bf626c99dbaf52ecfed96
siimongc <84151786+siimongc@users.noreply.github.com>
2022-03-30 09:57:01 -0500

    Update main.cpp
    lectura y ejecucion de los archivos jcoco

--- main.cpp
+++ main.cpp
@@ -1,10 +1,51 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <stdio.h>
+
+using namespace std;
+
+string filename;
+string line;
+int cont = 1;
 
 int main(){
 
+    while(cont>0){
 
-
+        cout<<"insert filename:"<<endl;
+        cin>>filename;
+        ifstream file(filename);
+        
+        if(file.is_open()){
+            
+            while(getline(file,line)){
+                cout<<line<<endl;
+            }
+            file.close();
+            cont--;
+        }
+        else{
+            cout<<"file is ot open, please insert a correct file"<<endl;
+        }
+    }
+    if(filename=="pascal.casm"){
+        system("java -jar JCoCo.jar pascal.casm");
+    }
+    else if(filename=="factorial.casm"){
+        system("java -jar JCoCo.jar factorial.casm");
+    }
+    else if(filename=="operaciones.casm"){
+         system("java -jar JCoCo.jar operaciones.casm");
+    }
+    else if(filename=="lista,casm"){
+        system("java -jar JCoCo.jar lista.casm");
+    }
+   
+    
+    
     return 0;
 }
+
 
 
