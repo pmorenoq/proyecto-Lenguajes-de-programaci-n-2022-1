873ab82d15879bcce2438d33d8b57b6b986cd3da
siimongc <84151786+siimongc@users.noreply.github.com>
2022-04-18 10:16:04 -0500

    Update Files.cpp

--- Files.cpp
+++ Files.cpp
@@ -4,7 +4,7 @@
 #include <stdio.h>
 #include <string>
 
-std::string Files::read(std::string filename, std::string line, std::string code){
+std::string Files::readOperaciones(std::string filename, std::string line, std::string code){
     std::string aux;
     char index;
     std::ifstream file(filename);
@@ -39,6 +39,54 @@ std::string Files::read(std::string filename, std::string line, std::string code
                 continue;
             }
         }
+        std::cout<<code<<std::endl;
+        
+        }
+
+        else{
+            std::cout<<"error file is ot open"<<std::endl;
+        
+        }
+
+    std::string filenametemp("tmp.casm");
+    std::fstream file_out;
+
+    file_out.open(filenametemp, std::ios_base::out);
+    if (!file_out.is_open()) {
+        std::cout << "failed to open " << filenametemp << '\n';
+    } else {
+        file_out << code << std::endl;
+        std::cout << "Done Writing!" << std::endl;
+    }
+    return code;
+
+}
+
+std::string Files::readFactorial(std::string filename, std::string line, std::string code){
+    std::string aux;
+    char index;
+    std::ifstream file(filename);
+        
+        if(file.is_open()){
+            
+            while(getline(file,line)){
+                aux = line + "\n";
+                code = code + aux;
+            }
+            file.close();
+
+        std::cout<<"ingresa un numero numero: "<<std::endl;
+        std::cin>>index;
+        for(int i = 0; i < code.size();i++){
+            if(code[i]=='h'){
+                code[i] = index;
+            }
+            else{
+                continue;
+            }
+        
+        }
+
         std::cout<<code<<std::endl;
         
         }
